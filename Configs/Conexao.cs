161b9d4f8d94d7eafaa72e480c4bd24f571d88a9
Chupirisco <matheusrodriguesjulio@gmail.com>
2026-09-14 11:21:38 -0400

    gh

--- Configs/Conexao.cs
+++
@@ -1,7 +0,0 @@
-namespace AutoFrota.Configs;
-
-// Ponto reservado para a configuração do banco de dados quando o projeto evoluir.
-public static class Conexao
-{
-    public const string NomeBanco = "AutoFrota";
-}

c6d879fc530fda0f4ce8f7be5ea2baf9eb7897e4
eduardoluca2009 <eduardoluca2009@gmail.com>
2026-09-21 11:17:27 -0400

    Tabela Cliente
    Criei a tabela do cliente e cliente DAO. Contendo id, nome, cpf e etc.

---
+++ DAO/ClienteDAO.cs
@@ -0,0 +1,43 @@
+﻿using DriveX.Configs;
+using DriveX.Model;
+
+namespace DriveX.DAO;
+
+public class ClienteDAO
+{
+    private readonly Conexao _conexao;
+
+    public ClienteDAO(Conexao conexao)
+    {
+        _conexao = conexao;
+    }
+
+    public List<Cliente> Listar()
+    {
+        var lista = new List<Cliente>();
+
+        using var con = _conexao.GetConnection();
+        using var comando = con.CreateCommand();
+
+        comando.CommandText = "SELECT * FROM Clientes";
+
+        using var leitor = comando.ExecuteReader();
+
+        while (leitor.Read())
+        {
+            lista.Add(new Cliente
+            {
+                Id = Convert.ToInt32(leitor["id_cliente"]),
+                Nome = leitor["nome"].ToString() ?? "",
+                Cpf = leitor["cpf"].ToString() ?? "",
+                Telefone = leitor["telefone"].ToString() ?? "",
+                Email = leitor["email"].ToString() ?? "",
+                ModeloInteresse = leitor["modelo_interesse"].ToString() ?? "",
+                StatusAtendimento = leitor["status_atend"].ToString() ?? "",
+                Observacao = leitor["observacao"].ToString() ?? ""
+            });
+        }
+
+        return lista;
+    }
+}
---
+++ Model/Cliente.cs
@@ -0,0 +1,14 @@
+﻿namespace DriveX.Model
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Cpf { get; set; } = string.Empty;
+        public string Telefone { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string ModeloInteresse { get; set; } = string.Empty;
+        public string StatusAtendimento { get; set; } = string.Empty;
+        public string Observacao { get; set; } = string.Empty;
+    }
+}

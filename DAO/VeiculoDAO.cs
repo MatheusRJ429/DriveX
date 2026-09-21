1a69ed4501d43b8b36507b8dd3675a4d639439ad
Chupirisco <matheusrodriguesjulio@gmail.com>
2026-09-21 09:41:48 -0400

    Update VeiculoDAO.cs

--- DAO/VeiculoDAO.cs
+++ DAO/VeiculoDAO.cs
@@ -6,20 +6,44 @@ namespace DriveX.DAO;
 public class VeiculoDAO
 {
     private readonly Conexao _conexao;
-    public VeiculoDAO(Conexao conexao) => _conexao = conexao;
+
+    public VeiculoDAO(Conexao conexao)
+    {
+        _conexao = conexao;
+    }
 
     public List<Veiculo> Listar()
     {
         try
         {
             var lista = new List<Veiculo>();
+
             using var con = _conexao.GetConnection();
+
+            string sql = @"
+                SELECT
+                    id_carro AS id_vei,
+                    modelo AS modelo_vei,
+                    marca AS marca_vei,
+                    'Não informada' AS placa_vei,
+                    ano AS ano_vei,
+                    'Automóvel' AS categoria_vei,
+                    CASE
+                        WHEN status = 'disponivel' THEN 'Disponível'
+                        WHEN status = 'vendido' THEN 'Vendido'
+                        ELSE status
+                    END AS status_vei
+                FROM Carros;
+            ";
+
             using var comando = con.CreateCommand();
-            comando.CommandText = "SELECT * FROM veiculos";
+            comando.CommandText = sql;
+
             using var leitor = comando.ExecuteReader();
+
             while (leitor.Read())
             {
-                lista.Add(new Veiculo
+                var veiculo = new Veiculo
                 {
                     Id = Convert.ToInt32(leitor["id_vei"]),
                     Modelo = leitor.GetString("modelo_vei"),
@@ -28,10 +52,16 @@ public class VeiculoDAO
                     Ano = Convert.ToInt32(leitor["ano_vei"]),
                     Categoria = leitor.GetString("categoria_vei"),
                     Status = leitor.GetString("status_vei")
-                });
+                };
+
+                lista.Add(veiculo);
             }
+
             return lista;
         }
-        catch { throw; }
+        catch
+        {
+            throw;
+        }
     }
 }
